8cce58cdf6c2b60796957aff65eb38ad0bf45bae
tinnt <43608205+tin-nt@users.noreply.github.com>
2020-12-30 18:02:42 +0700

    update sql

---
+++ SQL/Ôn Thi/test1.sql
@@ -0,0 +1,19 @@
+--https://cafedev.vn/tu-hoc-sql-bai-tap-thuc-hanh-create-insert-update-xoa-du-lieu-trong-sql/
+
+--1
+-- 1.2 
+alter table SANPHAM add GHICHU varchar(20)
+-- 1.3
+alter table KHACHHANG add LOAIKH tinyint
+-- 1.4
+update KHACHHANG
+set hoten = "Nguyen Van B"
+where makh = "KH01"
+-- 1.5
+set dateformat dmy
+update KHACHHANG
+set hoten = "Nguyen Van Hoan", ngdk = "14/01/2007" 
+where makh = "KH09"
+-- 1.6
+alter table SANPHAM alter column GHICHU varchar(100)
+
